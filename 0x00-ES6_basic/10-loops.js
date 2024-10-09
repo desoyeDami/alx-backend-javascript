43b2577b2930822ded5fc1cbc9a5b6fd3809d9f0
desoyedami <desoyedami@gmail.com>
2024-10-09 09:39:59 +0100

    create employees

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const [idx, value] of array.entries()) {
+    // eslint-disable-next-line no-param-reassign
     array[idx] = appendString + value;
   }
 
