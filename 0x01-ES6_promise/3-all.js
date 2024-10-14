303027f82729bb226504f5724544b97e7036c6a9
desoyedami <desoyedami@gmail.com>
2024-10-14 18:57:59 +0100

    handle profile

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,8 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(results[0].body, results[1].firstName, results[1].lastName);
+    })
+    .catch(() => {
+      console.error(new Error('Signup system offline'));
     });
-    // .catch(() => {
-        // console.error(new Error('Signup system offline'))})
 }
