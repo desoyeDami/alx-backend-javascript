e99fb73a426f330fb28f4cabfd6d41bba376a1e5
desoyedami <desoyedami@gmail.com>
2024-10-15 18:44:01 +0100

    file

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,9 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
+  return new Promise((resolve, reject) => {
+    if (filename) {
+      reject(new Error(`${filename} cannot be proessed`));
+    } else {
+      resolve();
+    }
   });
 }
