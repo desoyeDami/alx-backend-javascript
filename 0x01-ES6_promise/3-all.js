393ba3e5823b9ffc3136d01a0d49de54d75abf6a
desoyedami <desoyedami@gmail.com>
2024-10-16 20:21:00 +0100

    cleann

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js"
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    .then((results) => {
+      console.log(results[0].body, results[1].firstName, results[1].lastName);
     })
-    .catch(error => {
-      console.log('Signup system offline');
+    .catch(() => {
+      console.error(new Error('Signup system offline'));
     });
 }
